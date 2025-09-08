82a1a71f743212471b843a2f753fc490c0b7c8b4
Luiz Henrique <69431619+ryckmob@users.noreply.github.com>
2025-09-08 12:43:44 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
@@ -13,8 +13,23 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        typing: {
+          '0%': { width: '0ch' },
+          '100%': { width: '30ch' }, // ajusta conforme o tamanho do texto
+        },
+        blink: {
+          '0%, 100%': { borderColor: 'transparent' },
+          '50%': { borderColor: 'currentColor' },
+        },
+      },
+      animation: {
+        typing: 'typing 3s steps(30) 1s forwards',
+        blink: 'blink 1s step-end infinite',
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
